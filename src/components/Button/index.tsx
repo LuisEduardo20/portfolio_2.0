512ebfbf118d0c86b46cad9636e0389efbb038d2
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-10-30 21:54:38 -0300

    feat: add html button interface

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 
-const Button = () => {
-  return <button type="button">Button</button>;
+export interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const Button = ({ children, ...rest }: Props) => {
+  return (
+    <button type="button" data-testid="button" {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
