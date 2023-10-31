be18b8b3b255672cca2257840f764a56b9b1b96b
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-10-30 21:49:28 -0300

    test: button test

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,10 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Button from "./index";
 
 describe("<Button />", () => {
   it("should render the button", () => {
-    render(<Button />);
+    render(<Button>Button</Button>);
 
-    expect(screen.getByRole("button", { name: /Button/i })).toBeInTheDocument();
+    const element = screen.getByTestId("button");
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("should call the button function", () => {
+    const clickFn = jest.fn();
+
+    render(<Button onClick={clickFn}>Button</Button>);
+
+    const element = screen.getByTestId("button");
+
+    fireEvent.click(element);
+    expect(clickFn).toHaveBeenCalled();
   });
 });
